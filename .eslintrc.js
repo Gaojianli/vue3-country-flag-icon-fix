94297b6310f6b75ac0c18f01d646d5454a4bdfbc
Gaojianli <823805669hehe@gmail.com>
2024-05-12 03:41:51 +0800

    move to vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, './src/components/CountryFlag/Index.vue'),
+      name: 'CountryFlag',
+      // the proper extensions will be added
+      fileName: (format) => `CountryFlag.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
