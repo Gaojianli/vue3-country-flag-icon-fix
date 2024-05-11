f0ea17c11245880fbc07a1486bf246dcb9a0cfdd
Gaojianli <823805669hehe@gmail.com>
2024-05-12 04:21:00 +0800

    fix type

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
